8e4a480c5f5fc43acfa9d42883b679aa292ad7df
ian cousar <salambolog@gmail.com>
2016-09-14 10:08:53 -0400

    .env and gitignore added

--- app.js
+++ app.js
@@ -6,18 +6,20 @@ var connect = require('connect');
 var everyauth = require('everyauth');
 var path = require("path");
 
+app.listen(process.env.PORT || 5000);
 
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(__dirname + '/public'));
 
 
 app.set('view engine', 'html');
 app.set('view options', {layout: false});
 app.set('views', __dirname + '/');
 app.engine('html', require('ejs').renderFile);
-// app.set('view engine', 'html');
 
 app.get('/',function(req,res) {
   res.render('index');
 });
 
-app.listen(process.env.PORT || 5000);
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
